3d516595ba3ba29562df45c100669883a1d2f3eb
DeekshyaMohapatra <75194107+DeekshyaMohapatra@users.noreply.github.com>
2021-01-17 14:15:21 +0530

    Add files via upload

---
+++ js/Stone.js
@@ -0,0 +1,31 @@
+class Stone
+{
+    constructor(x,y,r)
+    {
+      var options=
+      {
+         isStatic:false,
+         'restitution': 0 ,
+         'friction':1,
+         'density':1.2 
+      }
+      this.body = Bodies.circle(x,y,r/2,options);
+      this.r = r;
+      this.image = loadImage("images/stone.png");
+      World.add(world,this.body);
+    }
+
+
+
+    display()
+    {
+      
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        imageMode(CENTER);
+        image(this.image,0,0,this.r*2,this.r*2); 
+        ellipseMode(RADIUS);
+      
+    }
+};
